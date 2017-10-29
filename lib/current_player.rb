42eddfd1f0dfcc99fe96c080202a04aa06cd9d46
Man Hong Lee <manhong323@gmail.com>
2017-10-29 01:26:54 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,7 +12,8 @@ def current_player(board)
   if turn_count(board) % 2 == 0 || turn_count(board) == 0
     "X"
   else
-  "O"
+    "O"
+  end
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
